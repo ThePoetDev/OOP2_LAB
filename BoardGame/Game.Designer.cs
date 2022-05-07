6b7064811b79c5005cc5ec50a75f829c9efdd288
feyzatesoglu <atesogluf35@gmail.com>
2022-05-07 22:58:50 +0300

    Game design is changed, added movement, added triangle and shape methods.

--- BoardGame/Game.Designer.cs
+++ BoardGame/Game.Designer.cs
@@ -1,6 +1,7 @@
-﻿
-namespace BoardGame {
-    partial class Game {
+﻿namespace BoardGame
+{
+    partial class Game
+    {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -10,8 +11,10 @@ namespace BoardGame {
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing) {
-            if (disposing && (components != null)) {
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -23,30 +26,52 @@ namespace BoardGame {
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent() {
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.GameBoard = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.GameBoard)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BoardGame.Properties.Resources.GreenTriangle;
+            this.pictureBox1.Location = new System.Drawing.Point(873, 254);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // GameBoard
             // 
             this.GameBoard.BackColor = System.Drawing.Color.SkyBlue;
+            this.GameBoard.ErrorImage = global::BoardGame.Properties.Resources.RedTriangle;
             this.GameBoard.Location = new System.Drawing.Point(12, 12);
+            this.GameBoard.Margin = new System.Windows.Forms.Padding(4);
             this.GameBoard.Name = "GameBoard";
-            this.GameBoard.Size = new System.Drawing.Size(540, 540);
+            this.GameBoard.Size = new System.Drawing.Size(775, 775);
             this.GameBoard.TabIndex = 0;
             this.GameBoard.TabStop = false;
             // 
+            // timer1
+            // 
+            // 
             // Game
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SkyBlue;
-            this.ClientSize = new System.Drawing.Size(902, 623);
+            this.ClientSize = new System.Drawing.Size(1047, 828);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.GameBoard);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Game";
             this.Text = "Game";
             this.Load += new System.EventHandler(this.Game_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.GameBoard)).EndInit();
             this.ResumeLayout(false);
 
@@ -55,5 +80,7 @@ namespace BoardGame {
         #endregion
 
         private System.Windows.Forms.PictureBox GameBoard;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
